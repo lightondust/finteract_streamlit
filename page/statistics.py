f4dd817b1d6a485ed1ee64ea03b232c23d874557
zs <lightondust@gmail.com>
2021-01-23 21:58:33 +0900

    select both from code and name

--- page/statistics.py
+++ page/statistics.py
@@ -9,7 +9,7 @@ fields = ['時価総額', '従業員数', '総収入', '純利益']
 def statistics(st: streamlit, data_df: DataFrame):
     st.title('データ分布')
 
-    target_type = st.radio('対象銘柄', ['全体', 'セクター別', '銘柄を選択する'])
+    target_type = st.radio('対象銘柄', ['全体', 'セクター別', '銘柄を個別指定'])
     show_df = select_stocks(st, target_type, data_df)
 
     field = st.selectbox('表示項目', fields)
@@ -39,7 +39,9 @@ def display_statistics_2d(st: streamlit, show_df: DataFrame, field1: str, field2
                          log_x=log_if,
                          log_y=log_if,
                          marginal_x='violin',
-                         marginal_y='violin')
+                         marginal_y='violin',
+                         labels=show_df.display_name,
+                         hover_name='display_name')
 
     st_fig.plotly_chart(fig)
 
@@ -50,33 +52,33 @@ def display_statistics_1d(st: streamlit, show_df: DataFrame, field: str):
 
     select_type = st.radio('グラフ種類', ['棒グラフ', 'ヒストグラム', 'ツリーマップ'])
     if select_type == '棒グラフ':
-        fig = px.bar(show_df.sort_values(field, ascending=False).iloc[:100], x='名前', y=field, log_y=log_if)
+        fig = px.bar(show_df.sort_values(field, ascending=False).iloc[:100], x='display_name', y=field, log_y=log_if)
     elif select_type == 'ヒストグラム':
         fig = px.histogram(show_df, x=field, log_y=log_if)
     elif select_type == 'ツリーマップ':
         show_df_ = show_df.sort_values(field, ascending=False)
-        fig = px.treemap(show_df_, values=field, path=['セクター', '名前'], labels=show_df_['名前'])
+        fig = px.treemap(show_df_, values=field, path=['セクター', 'display_name'], labels=show_df_['display_name'])
 
     st_fig.plotly_chart(fig)
 
 
 def select_stocks(st, target_type, df_to_filter):
 
+    stocks = df_to_filter.display_name.to_list()
     if target_type == 'セクター別':
-        from_sector_name_or_code = st.radio('選択方法', ['セクター名', '銘柄から所属セクター'])
-        if from_sector_name_or_code == '銘柄から所属セクター':
-            code_list = df_to_filter['code'].to_list()
-            code = st.selectbox('証券コード', code_list)
-            company_data = df_to_filter[df_to_filter['code'] == code].iloc[0]
+        from_sector_name_or_stock = st.radio('選択方法', ['セクター名', '銘柄から所属セクター'])
+        if from_sector_name_or_stock == '銘柄から所属セクター':
+            stock = st.selectbox('証券コード', stocks)
+            company_data = df_to_filter[df_to_filter.display_name == stock].iloc[0]
             sector = company_data['セクター']
         else:
             sectors = list(set(df_to_filter['セクター'].to_list()))
             sector = st.selectbox('セクター', sectors)
+        st.markdown('### セクター：{}'.format(sector))
         show_df = df_to_filter[df_to_filter['セクター'] == sector]
-    elif target_type == '銘柄を指定':
-        code_list = df_to_filter['code'].to_list()
-        code_list_selected = st.multiselect('code', code_list)
-        show_df = df_to_filter[df_to_filter.code.isin(code_list_selected)].sort_values('時価総額', ascending=False)
+    elif target_type == '銘柄を個別指定':
+        stocks_selected = st.multiselect('銘柄：', stocks)
+        show_df = df_to_filter[df_to_filter.display_name.isin(stocks_selected)].sort_values('時価総額', ascending=False)
     else:
         show_df = df_to_filter
 
