c8a61e385588d15493677eafa23f3bc3e407ee0f
zs <lightondust@gmail.com>
2021-01-23 21:34:40 +0900

    display 2nd variable

--- page/statistics.py
+++ page/statistics.py
@@ -7,16 +7,48 @@ fields = ['時価総額', '従業員数', '総収入', '純利益']
 
 
 def statistics(st: streamlit, data_df: DataFrame):
-    st.title('分布')
+    st.title('データ分布')
 
     target_type = st.radio('対象銘柄', ['全体', 'セクター別', '銘柄を選択する'])
-    show_df = filter_data(st, target_type, data_df)
+    show_df = select_stocks(st, target_type, data_df)
 
-    field = st.selectbox('表示する項目', fields)
+    field = st.selectbox('表示項目', fields)
+    if_2d = st.checkbox('２つめの項目')
 
+    if if_2d:
+        field_second = st.selectbox('表示項目2', fields)
+        display_statistics_2d(st, show_df, field, field_second)
+    else:
+        display_statistics_1d(st, show_df, field)
+
+    if_map = st.checkbox('本社所在地を地図で見る')
+    if if_map:
+        display_on_map(st, show_df)
+
+
+def display_statistics_2d(st: streamlit, show_df: DataFrame, field1: str, field2: str):
     st_fig = st.empty()
     log_if = st.checkbox('対数スケール', key='log_total')
-    select_type = st.radio('種類', ['棒グラフ', 'ヒストグラム', 'ツリーマップ'])
+
+    select_type = st.radio('グラフ種類', ['散布図'])
+
+    if select_type == '散布図':
+        fig = px.scatter(show_df,
+                         x=field1,
+                         y=field2,
+                         log_x=log_if,
+                         log_y=log_if,
+                         marginal_x='violin',
+                         marginal_y='violin')
+
+    st_fig.plotly_chart(fig)
+
+
+def display_statistics_1d(st: streamlit, show_df: DataFrame, field: str):
+    st_fig = st.empty()
+    log_if = st.checkbox('対数スケール', key='log_total')
+
+    select_type = st.radio('グラフ種類', ['棒グラフ', 'ヒストグラム', 'ツリーマップ'])
     if select_type == '棒グラフ':
         fig = px.bar(show_df.sort_values(field, ascending=False).iloc[:100], x='名前', y=field, log_y=log_if)
     elif select_type == 'ヒストグラム':
@@ -27,12 +59,8 @@ def statistics(st: streamlit, data_df: DataFrame):
 
     st_fig.plotly_chart(fig)
 
-    if_map = st.checkbox('本社所在地を地図で見る')
-    if if_map:
-        display_on_map(st, show_df)
 
-
-def filter_data(st, target_type, df_to_filter):
+def select_stocks(st, target_type, df_to_filter):
 
     if target_type == 'セクター別':
         from_sector_name_or_code = st.radio('選択方法', ['セクター名', '銘柄から所属セクター'])
