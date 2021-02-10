91a6ee443080fad5453421cd5158606562362a9e
zs <lightondust@gmail.com>
2021-02-10 22:07:58 +0900

    change url

--- page/navigation.py
+++ page/navigation.py
@@ -2,7 +2,17 @@ import streamlit
 from page_manager import PAGE_ORDER
 
 
-def navigation(st: streamlit):
-    page = st.sidebar.radio('画面選択', PAGE_ORDER)
-    st.experimental_set_query_params(page=page)
-    return page
+def navigation(st: streamlit, page: list):
+
+    page_idx_default = 0
+    if page:
+        page_idx_url = int(page[0])
+        if page_idx_url in list(range(len(PAGE_ORDER))):
+            page_idx_default = page_idx_url
+
+    page_selected = st.sidebar.radio('画面選択', PAGE_ORDER, index=page_idx_default)
+
+    page_idx = PAGE_ORDER.index(page_selected)
+    st.experimental_set_query_params(page=page_idx)
+
+    return page_selected
--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,9 @@ from pre_process import read_company_info
 st.text('チュートリアル2、使い勝手改善：サイドバー、モジュール分け、プレースホルダー')
 
 company_info_df = read_company_info()
-page = navigation(st)
 
-PAGE_MAP[page](st, company_info_df)
+params = st.experimental_get_query_params()
+
+page_to_display = navigation(st, page=params.get('page'))
+
+PAGE_MAP[page_to_display](st, company_info_df)
