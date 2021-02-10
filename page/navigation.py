56d4c26a6a2e9f69c13a639955e5ea64bfeda86a
zs <lightondust@gmail.com>
2021-02-10 22:20:41 +0900

    abort url rewriting

--- page/navigation.py
+++ page/navigation.py
@@ -4,5 +4,4 @@ from page_manager import PAGE_ORDER
 
 def navigation(st: streamlit):
     page = st.sidebar.radio('画面選択', PAGE_ORDER)
-    st.experimental_set_query_params(page=page)
     return page
