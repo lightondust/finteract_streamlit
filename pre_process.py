359a425ca1df1a03d7e6092dc872082f2036ac9a
zs <lightondust@gmail.com>
2021-07-14 17:16:36 +0900

    fix bug of tree map

--- pre_process.py
+++ pre_process.py
@@ -8,7 +8,7 @@ def read_company_info():
     print('read data')
     company_info_path = os.path.join('.', 'data', 'stock_map.csv')
     df = pd.read_csv(company_info_path, index_col=0)
-    df['display_name'] = df.code.apply(lambda x: '{} '.format(x)) + df['名前']
+    df['display_name'] = df.code.apply(lambda x: '{} '.format(x)) + df['name']
     df['セクター'] = df['セクター'].fillna('-')
     df['セクター_en'] = df['セクター_en'].fillna('-')
     df[['時価総額', '総収入', '純利益']] = to_oku(df[['時価総額', '総収入', '純利益']])
